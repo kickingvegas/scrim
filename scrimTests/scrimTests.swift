573b0cf3528c296e33d1f0e0e46aa08ce935c534
Charles Choi <kickingvegas@gmail.com>
2025-03-31 11:37:34 -0700

    Initial Commit

---
+++ scrimTests/scrimTests.swift
@@ -0,0 +1,17 @@
+//
+//  scrimTests.swift
+//  scrimTests
+//
+//  Created by Charles Choi on 3/31/25.
+//
+
+import Testing
+@testable import scrim
+
+struct scrimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
